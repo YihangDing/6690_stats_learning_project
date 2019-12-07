809ce9f2a36f4606bfad905d0724a54adeffb255
aquacola <yd2459@columbia.edu>
2019-12-07 18:14:15 -0500

    modified line_cnter

--- line_cnter.py
+++ line_cnter.py
@@ -1,4 +1,4 @@
-with open('covtype.data') as f:
+with open('dataset_diabetes/diabetic_data.csv') as f:
     lines = f.readlines()
     print('num of rows: {}'.format(len(lines)))
     first_line = lines[0].split(',')
